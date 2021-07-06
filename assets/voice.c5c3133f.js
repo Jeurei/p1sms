3590c9be964c55492c50922ee5e3c24a54084083
Daniel Read0nly <ihatemat@yandex.ru>
2021-07-06 16:38:03 +0500

    deploy

---
+++ assets/voice.c5c3133f.js
@@ -0,0 +1,1 @@
+import"./main.eed65435.js";import"./vendor.64dcf12d.js";
---
+++ sms-tele2.php
@@ -0,0 +1,174 @@
+    <?php include 'header.php' ?>
+    <title>СМС-рассылка для бизнеса по абонентам МТС - P1sms</title>
+    <meta name="description" content="СМС-рассылка абонентам МТС по самым выгодным ценам. Отправляйте до 100 тыс. смс в день из личного кабинета. Смотрите статистика рассылок в реальном времени. "/>
+    <style type="text/css">
+   TABLE {
+    width: 300px; 
+    border: 1px solid #D6E0EE; 
+   }
+   TD, TH {
+    padding: 1px;
+    border: 1px solid #D6E0EE; 
+   }
+   TH {
+    text-align: left; 
+    background: white; 
+    color: black;
+    font-family: 'PF DinText Pro' 
+   }
+  </style>
+    </head>
+<body
+    class="page-template-default page page-id-2405 page-child parent-pageid-303 wp-custom-logo no-sidebar-full-width wpb-js-composer js-comp-ver-5.0.1 vc_responsive">
+<?php include 'menu.php' ?>
+<div class="clearfix" id="main">
+    <div class="inner-wrap2">
+        <div id="primary">
+            <div class="clearfix" id="content">
+                <article class="post-2405 page type-page status-publish hentry" id="post-2405">
+                    <div class="entry-content clearfix">
+                        <div class="wpb_row vc_row-fluid ult-vc-hide-row vc_row-has-fill">
+
+                            <div class="section service-page upb_row_bg">
+                                <div class="inner-wrap">
+                                    <div class="row">
+                                        <div class="col-md-7">
+                                            <h1 class="h1">СМС-рассылка для бизнеса<br> по абонентам МТС </h1>
+
+                                            <div class="service-page_description">
+                                                <p>
+                                                    Платите только за доставленные SMS. <br> От 1,44 руб. за сообщение.</p>
+                                                    <a class="button" style="margin-top: 20px;"
+                                       href="https://admin.p1sms.ru/ru-ru/registration.html">
+                                        Запустить рассылку</a>
+
+                                            </div>
+                                        </div>
+                                        <div  class="col-md-5">
+                                            <img class="service-page_img api--img" alt="смс рассылки по абонентам мтс"
+                                                 src="../../../wp-content/themes/spacious/css/img/sms-mts-1.jpg">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="background-color: #F2F9FF" class="wpb_row vc_row-fluid ult-vc-hide-row vc_row-has-fill">
+                            <div style="padding-bottom: 15px !important" class="section voice-why upb_row_bg">
+                                <div style="width: 50%" class="inner-wrap" style="padding-left:0px">
+                                    <h2 style="text-align: center; margin-top: 55px; margin-bottom: 25px !important;" class="h1 ebaboba">
+                                       
+                                    </h2>
+                                    
+                                    <div class="row api-doc">
+                                        <p>Наш сервис позволяет запустить СМС-рассылку по абонентам МТС вашей клиентской базы. Вы платите только за доставленные SMS-сообщения. Благодаря этому, ваш бизнес экономит до 30% на оповещении клиентов. У нас прямое подключение к МТС + мы предоставляем беcплатные общие имена отправителей.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <div  class="wpb_row vc_row-fluid ult-vc-hide-row vc_row-has-fill">
+                            <div class="section voice-why upb_row_bg">
+                                <div class="inner-wrap" style="padding-left:0px">
+                                    <h2 style="text-align: center; margin-top: 55px; margin-bottom: 25px !important;" class="h1 ebaboba">
+                                       Особенности СМС-рассылок
+                                    </h2>
+                                    <div class="row">
+                                        <div class="col-md-4 voice-why_block">
+                                            <img class="voice-why_img" alt="Преимущества рассылок по мтс"
+                                                 src="../../wp-content/themes/spacious/css/img/kaskad-ico-1-1.png"><br>
+                                            <p style="margin-top: 10px;margin-bottom: 10px; font-weight: bolder">Бесплатный тест</p>
+                                            <p>Благодаря бесплатным смс-пакетам, вы можете протестировать все функции нашего сервиса.</p>
+                                        </div>
+                                        <div class="col-md-4 voice-why_block">
+                                            <img class="voice-why_img" alt="Преимущества рассылок по мтс"
+                                                 src="../../wp-content/themes/spacious/css/img/kaskad-ico2.png"><br>
+                                            <p style="margin-top: 10px;margin-bottom: 10px; font-weight: bolder">Выгодно</p>
+                                            <p>Прямое подключение к МТС позволяет предоставлять выгодные тарифы на рассылку.</p>
+                                        </div>
+                                        <div class="col-md-4 voice-why_block">
+                                            <img class="voice-why_img" alt="Преимущества рассылок по мтс"
+                                                 src="../../wp-content/themes/spacious/css/img/kaskad-ico-2-1.png"><br>
+                                            <p style="margin-top: 10px;margin-bottom: 10px; font-weight: bolder">Удобство</p>
+                                            <p>Создавайте список МТС номеров в Excel и загружайте его к нам на сайт в одно нажатие.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                         <div style="background-color: #F2F9FF" class=" vc_row-fluid ult-vc-hide-row vc_row-has-fill">
+                            <div class="section voice-why upb_row_bg">
+                                <div class="inner-wrap" style="padding-left:0px">
+                                    <h2 style="text-align: center; margin-top: 55px; margin-bottom: 25px !important;" class="h1 ebaboba">
+                                        Тарифы смс-рассылки по абонентам МТС
+                                    </h2>
+                                    <div style="padding-bottom: 0px !important" class="row api-doc">
+                                                              <table class="price_table">
+                                                                <tbody>
+                                                                <tr>
+                                                                    <th style="text-align: left; padding: 0 50px; font-weight: bolder; font-size: 25px;" colspan="2">Рассылка<br><span style="font-size: 16px;">с персональным именем</span>
+                                                                    </th>
+                                                                    <th style="font-size: 35px" colspan="2"><span style="color: #5181B8"><span style="color: #5181B8">от 
+                                                                    1,44 ₽</span><span style="color: #EB5757">*</span>
+                                                                    </th>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th style="text-align: left; padding: 0 50px; font-weight: bolder; font-size: 25px;" colspan="2">Рассылка<br><span style="font-size: 16px;">с общем именем</span>
+                                                                    </th>
+                                                                    <th style="font-size: 35px" colspan="2"><span style="color: #5181B8"><span style="color: #5181B8">от 1,75 ₽</span><span style="color: #EB5757">*</span>
+                                                                    </th>
+                                                               
+                                                                
+                                                                </tbody>
+                                                            </table>
+                                                            <p><span style="color: #EB5757">*</span>Стоимость указана с НДС</p>
+                                                            <a class="button" style="margin-top: 20px;"
+                                       href="https://admin.p1sms.ru/ru-ru/registration.html">
+                                        Запустить рассылку</a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       <div>
+                            <div  class="wpb_row vc_row-fluid ult-vc-hide-row vc_row-has-fill">
+                            <div class="section voice-why upb_row_bg">
+                                <div class="inner-wrap" style="padding-left:0px">
+                                    <h2 style="text-align: center; margin-top: 55px; margin-bottom: 25px !important;" class="h1 ebaboba">
+                                      Возможности для бизнеса
+                                    </h2>
+                                    <div class="row">
+                                        <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12 voice-why_block" align="center">
+                                            <img class="voice-why_img" alt="Возможности использования рассылки для бизнеса"
+                                                 src="../../wp-content/themes/spacious/css/img/sms-for-business-1-1.png"><br>
+                                           
+                                            <p>Сервисное оповещение клиентов о статусе заказа, доставки, оплаты и т.д </p>
+                                        </div>
+                                        <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12 voice-why_block" align="center">
+                                            <img class="voice-why_img" alt="Возможности использования рассылки для бизнеса"
+                                                 src="../../wp-content/themes/spacious/css/img/sms-for-business-1-2.png"><br>
+                                          
+                                            <p>Рекламная рассылка: начало распродажи, новые товары, услуги и т.д </p>
+                                        </div>
+                                        <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12 voice-why_block" align="center">
+                                            <img class="voice-why_img" alt="Возможности использования рассылки для бизнеса"
+                                                 src="../../wp-content/themes/spacious/css/img/sms-for-business-1-3.png"><br>
+                                           
+                                            <p>Опросы клиентов для улучшение качества сервиса</p>
+                                        </div>
+                                        <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12 voice-why_block" align="center">
+                                            <img class="voice-why_img" alt="Возможности использования рассылки для бизнеса"
+                                                 src="../../wp-content/themes/spacious/css/img/sms-for-business-1-4.png"><br>
+    
+                                            <p>Мгновенное оповещение сотрудников внутри компании</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                      
+                </article>
+            </div><!-- #content -->
+        </div><!-- #primary -->
+    </div><!-- .inner-wrap -->
+</div><!-- #main -->
+<?php include 'shiras2.php' ?>
+<?php include 'oplata.php' ?>
+<?php include 'footer.php' ?>
